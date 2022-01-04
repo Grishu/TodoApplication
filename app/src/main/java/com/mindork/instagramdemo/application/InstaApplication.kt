57ddfe02b78ebbbf80ed5db6ccee3a37a4386818
grish <grishma_9879@yahoo.com>
2022-01-04 10:56:09 +0800

    Updates Commit

--- app/src/main/java/com/mindork/instagramdemo/application/InstaApplication.kt
+++ app/src/main/java/com/mindork/instagramdemo/application/InstaApplication.kt
@@ -2,7 +2,7 @@ package com.mindork.instagramdemo.application
 
 import android.app.Application
 import com.androidnetworking.AndroidNetworking
-
+// Main application class file
 class InstaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
